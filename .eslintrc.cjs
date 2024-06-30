63d230baf5d82d180e344a8e58fd95f113b1a852
Alireza Akbarzadeh <Alireza.Akbarzadeh.dev@gmail.com>
2024-06-30 14:50:15 +0330

    feat: intall components

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,5 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
@@ -17,5 +17,8 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off'
   }
-}
+};
--- src/components/ui/menubar/MenubarSub.vue
+++ src/components/ui/menubar/MenubarSub.vue
@@ -1,18 +1,17 @@
 <script setup>
-import { PopoverRoot, useForwardPropsEmits } from 'radix-vue';
+import { MenubarSub, useForwardPropsEmits } from "radix-vue";
 
 const props = defineProps({
   defaultOpen: { type: Boolean, required: false },
   open: { type: Boolean, required: false },
-  modal: { type: Boolean, required: false }
 });
-const emits = defineEmits(['update:open']);
+const emits = defineEmits(["update:open"]);
 
 const forwarded = useForwardPropsEmits(props, emits);
 </script>
 
 <template>
-  <PopoverRoot v-bind="forwarded">
+  <MenubarSub v-bind="forwarded">
     <slot />
-  </PopoverRoot>
+  </MenubarSub>
 </template>
